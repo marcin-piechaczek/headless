6d4c900e89951f80566862d67e42c2971e11900d
marcin.piechaczek <mpiechaczek@o2.pl>
2020-11-30 13:54:26 +0100

    ddd

--- hooks/useTidioChat.js
+++ hooks/useTidioChat.js
@@ -4,7 +4,7 @@ const useTidioChat = () => {
   useEffect(() => {
     const script = document.createElement('script');
 
-    script.src = '//code.tidio.co/8lg9h4i9tbx7k5yo2ws4m5hhbtaidmjj.js';
+    script.src = 'http://widget-v4.tidiochat.com/1_44_2/static/js/render.25f149d94e7f5d0c1136.js';
     script.async = true;
 
     document.body.appendChild(script);
